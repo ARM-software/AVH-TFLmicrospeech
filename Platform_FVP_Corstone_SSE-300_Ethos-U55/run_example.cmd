bd08130cbc30ea6afd26ed252f5c5d064b98fa25
armflorentlebeau <64065901+armflorentlebeau@users.noreply.github.com>
2022-01-28 13:54:52 +0100

    Update run_example.cmd

--- Platform_FVP_Corstone_SSE-300_Ethos-U55/run_example.cmd
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/run_example.cmd
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\ARM\VHT\models\Win64_VC2019\VHT_Corstone_SSE-300_Ethos-U55" -V "..\VSI\audio\python" -f fvp_config.txt -a Objects\microspeech.axf --stat --cyclelimit 2400000000 %*
+"C:\Program Files\ARM\VHT\models\Win64_VC2019\VHT_Corstone_SSE-300_Ethos-U55.exe" -V "..\VSI\audio\python" -f fvp_config.txt -a Objects\microspeech.axf --stat --cyclelimit 2400000000 %*
