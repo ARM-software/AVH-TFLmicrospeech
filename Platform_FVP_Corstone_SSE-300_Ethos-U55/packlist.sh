22bc9882f1f2ba4f394f5b0879f9668cd4b5bbe7
Matthias Hertel <matthias.hertel@arm.com>
2022-02-03 10:15:02 +0100

    Create packlist.sh

---
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/packlist.sh
@@ -0,0 +1,8 @@
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/tensorflow.flatbuffers.0.4.0.pack -a
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/tensorflow.kissfft.0.4.0.pack -a
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/tensorflow.ruy.0.4.0.pack -a 
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/tensorflow.gemmlowp.0.4.0.pack -a
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/tensorflow.tensorflow-lite-micro.0.4.0.pack -a
+cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/ARM.ethos-u-core-driver.0.4.0.pack -a
+cpackget pack add https://www.keil.com/pack/ARM.CMSIS.5.8.0.pack -a
+cpackget pack add https://keilpack.azureedge.net/pack/ARM.V2M_MPS3_SSE_300_BSP.1.2.0.pack -a
