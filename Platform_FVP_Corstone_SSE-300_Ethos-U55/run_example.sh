a1fd0f7542a123b1b111ea3d2fe47e86f3c50010
Robert Rostohar <robert.rostohar@arm.com>
2022-03-09 07:23:13 +0100

    Update run scripts for Corstone-300 FVP platform: faster simulation
    Use parameter simlimit instead of cyclelimit.
    
    This enables optimized simulation of idle mode (WFI) which results
    in ~10 times faster simulation.

--- Platform_FVP_Corstone_SSE-300_Ethos-U55/run_example.sh
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/run_example.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-VHT_Corstone_SSE-300_Ethos-U55 -V "../VSI/audio/python" -f fvp_config.txt -a Objects/microspeech.axf --stat --cyclelimit 2400000000 $*
+VHT_Corstone_SSE-300_Ethos-U55 -V "../VSI/audio/python" -f fvp_config.txt -a Objects/microspeech.axf --stat --simlimit 24 $*
