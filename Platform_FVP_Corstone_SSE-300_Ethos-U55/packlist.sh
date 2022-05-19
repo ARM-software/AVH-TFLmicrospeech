4dd61403b20e9c059cb9adcf45497c4cfe043a1a
armflorentlebeau <64065901+armflorentlebeau@users.noreply.github.com>
2022-05-19 17:17:00 +0200

    Return no error if packages are already installed

--- Platform_FVP_Corstone_SSE-300_Ethos-U55/packlist.sh
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/packlist.sh
@@ -6,3 +6,5 @@ cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download
 cpackget pack add https://github.com/MDK-Packs/tensorflow-pack/releases/download/0.4/ARM.ethos-u-core-driver.0.4.0.pack -a
 cpackget pack add https://www.keil.com/pack/ARM.CMSIS.5.8.0.pack -a
 cpackget pack add https://keilpack.azureedge.net/pack/ARM.V2M_MPS3_SSE_300_BSP.1.2.0.pack -a
+
+exit 0
