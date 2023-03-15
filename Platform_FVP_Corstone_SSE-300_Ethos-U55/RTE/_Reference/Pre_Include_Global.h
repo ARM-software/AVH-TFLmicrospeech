76ab3f0e7ac8ef8cdcb4f7e4dcec0c3e3c567a70
Robert Rostohar <robert.rostohar@arm.com>
2023-03-15 12:24:38 +0100

    Synchronize Example .cprj files (#16)

---
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/RTE/_Reference/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'microspeech' 
+ * Target:  'Reference' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* tensorflow::Machine Learning:TensorFlow:Kernel:Reference:1.22.5-rc4 */
+// enabling global pre includes 
+        #define TF_LITE_STATIC_MEMORY 1
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/RTE/_SIMD/RTE_Components.h
@@ -0,0 +1,44 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'microspeech' 
+ * Target:  'SIMD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SSE300MPS3.h"
+
+/* ARM::CMSIS Driver:USART:1.0.0 */
+#define RTE_Drivers_USART
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil.ARM Compiler::Compiler:Event Recorder:Semihosting:1.5.1 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+          #define RTE_Compiler_EventRecorder_Semihosting
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/* tensorflow::Data Exchange:Serialization:flatbuffers:tensorflow:1.22.5-rc4 */
+#define RTE_DataExchange_Serialization_flatbuffers     /* flatbuffers */
+/* tensorflow::Data Processing:Math:gemmlowp fixed-point:tensorflow:1.22.5-rc4 */
+#define RTE_DataExchange_Math_gemmlowp     /* gemmlowp */
+/* tensorflow::Data Processing:Math:kissfft:tensorflow:1.22.5-rc4 */
+#define RTE_DataExchange_Math_kissfft     /* kissfft */
+/* tensorflow::Data Processing:Math:ruy:tensorflow:1.22.5-rc4 */
+#define RTE_DataProcessing_Math_ruy     /* ruy */
+/* tensorflow::Machine Learning:TensorFlow:Kernel:CMSIS-NN:1.22.5-rc4 */
+#define RTE_ML_TF_LITE     /* TF */
+
+
+#endif /* RTE_COMPONENTS_H */
