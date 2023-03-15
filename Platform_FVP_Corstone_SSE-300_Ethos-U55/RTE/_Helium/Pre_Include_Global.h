8ee833ce7527c10198d44a531e6a475c25149252
Robert Rostohar <robert.rostohar@arm.com>
2023-03-15 11:31:30 +0100

    Synchronize Example .cprj files

---
+++ Platform_FVP_Corstone_SSE-300_Ethos-U55/RTE/_Helium/Pre_Include_Global.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'microspeech' 
+ * Target:  'Helium' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* tensorflow::Machine Learning:TensorFlow:Kernel:CMSIS-NN:1.22.5-rc4 */
+// enabling global pre includes 
+        #define TF_LITE_STATIC_MEMORY 1
+        #define CMSIS_NN
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
