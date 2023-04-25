caefafd521731d731e7b2a5c99308fb4e7ab36ec
Matthias Hertel <matthias.hertel@arm.com>
2023-04-25 14:46:15 +0200

    Change target name

---
+++ Platform_MIMXRT1064-EVK/RTE/_Debug_MIMXRT1064/Pre_Include_Global.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Debug+MIMXRT1064' 
+ * Target:  'Debug+MIMXRT1064' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* tensorflow::Machine Learning:TensorFlow:Kernel:CMSIS-NN:1.22.5-rc4 */
+// enabling global pre includes 
+        #define TF_LITE_STATIC_MEMORY 1
+        #define CMSIS_NN
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
